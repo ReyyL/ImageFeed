a7285df3302f5837a00b7727c98fe93a19361f68
Andrey Lazarev <a.n.lazarev@tinkoff.ru>
2024-03-28 18:31:43 +0500

    Добавил навигацию между экранами и скроллвью

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -11,9 +11,20 @@ final class ImagesListCell: UITableViewCell {
     
     static let reuseIdentifier = "ImagesListCell"
     
-    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet private weak var likeButton: UIButton!
     
-    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
     
-    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet private weak var cellImage: UIImageView!
+    
+    func configureCell(imageFromArray: UIImage?, date: String, isLiked: Bool) {
+        
+        cellImage.image = imageFromArray
+        
+        dateLabel.text = date
+        
+        likeButton.imageView?.image = isLiked
+        ? UIImage(named: "like_button_on")
+        : UIImage(named: "like_button_off")
+    }
 }
